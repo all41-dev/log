99712e0b7ac2b8ac6b49ba00ab8e5ea18ef2fefe
Néville Dubuis <neville@harps>
2024-01-15 15:24:24 +0100

    fix(sql): separate files

--- src/models/scripts/v1.0.0.sql
+++ src/models/scripts/v1.0.0.sql
@@ -9,14 +9,3 @@ create table logEntry
 	deletedAt timestamp null,
 	updatedAt timestamp default '0000-00-00 00:00:00' not null
 );
-
-create table meta
-(
-	logEntryUuid char(36) not null,
-	`key` varchar(200) not null,
-	value varchar(200) not null,
-	constraint meta_logEntry_id_fk
-		foreign key (logEntryUuid) references logEntry (id)
-			on update cascade on delete cascade
-);
-
--- src/models/scripts/v1.0.1.sql
+++ src/models/scripts/v1.0.1.sql
@@ -1,4 +1,9 @@
-alter table logEntry modify message longtext null;
-alter table logEntry modify callStack longtext null;
-
-alter table meta modify `value` longtext null;
+create table meta
+(
+	logEntryUuid char(36) not null,
+	`key` varchar(200) not null,
+	value varchar(200) not null,
+	constraint meta_logEntry_id_fk
+		foreign key (logEntryUuid) references logEntry (id)
+			on update cascade on delete cascade
+);
