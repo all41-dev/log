af7fb22083e59c4da43050f2536f4647765ce8b2
Néville Dubuis <neville@harps.ch>
2026-07-09 01:03:36 +0200

    fix linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,12 +29,6 @@ export default [
     files: ["test/**/*.{js,mjs,cjs,ts}"]
   },
   {
-    languageOptions: {
-      globals: globals.node
-    }
-  },
-  ...tseslint.configs.recommended,
-  {
     plugins: {
       '@stylistic/ts': stylisticJs
     },
